80ad2c54b1baf1a1c040b87bfccf85171cb9dcc6
Jen Hoskins <jen.c.hoskins@gmail.com>
2018-09-14 16:08:55 +0100

    Added margin to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    return <h1 className="display-4 text-center">Aden, Andy and Jen Help You Do Things</h1>
+    return <h1 className="display-4 text-center my-5">Aden, Andy and Jen Help You Do Things</h1>
 }
 
 export default Header;
